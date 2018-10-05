90a19af3383276f0717a83b16e092b85792a0f82
Mg Apyar <mgapyar@gmail.com>
2018-10-05 17:02:52 +0630

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var test = document.getElementsByClassName("snippets");
 for(var i=0;i<test.length;i++){
-test.item(i).innerText = "Hello..\nClick to view this post!";
+test.item(i).innerText = "Hello..\nClick to view this post!\nMgAPyar is NOT associated or affiliated with Google, Google Play or Android in any way. All the apps & games are property and trademark of their respective developer or publisher. ";
 }
